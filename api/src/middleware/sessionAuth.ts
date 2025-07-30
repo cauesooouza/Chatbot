cf4a01ba077f7f0870abb5f8941176694fb8307a
Caue Souza <caue.souzapl@gmail.com>
2025-07-30 16:24:09 -0300

    feat(auth): enhance sessionAuth middleware with async handling and standardized error responses

--- api/src/middleware/sessionAuth.ts
+++ api/src/middleware/sessionAuth.ts
@@ -1,14 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import { AuthService } from "../modules/Auth/auth.service.js";
+import { GenericError, HttpStatus } from "../errors/GenericError.js";
+import jwt from 'jsonwebtoken';
+import { config } from "../config/config.js";
 
-export const sessionAuth = (req: Request, res: Response, next: NextFunction) => {
+export const sessionAuth = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1];
-    if (!token || !(AuthService.isSessionActive(token))) {
-        res.status(401).json({
-            statusCode: 400,
-            messageError: "Session expired"
-        });
-        return;
+    if (!token) {
+        throw new GenericError("Token expired or invalid", HttpStatus.UNAUTHORIZED);
     }
-    next();
+
+    try {
+        jwt.verify(token, config.apiSecret);
+        const isActive = await AuthService.isSessionActive(token);
+        if (!isActive) throw new GenericError("Token expired or invalid", HttpStatus.UNAUTHORIZED);
+        next();
+    } catch (error) {
+        console.error(error);
+        throw new GenericError("Bad Request", HttpStatus.BAD_REQUEST)
+    }
 }
