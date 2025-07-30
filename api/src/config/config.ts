f6c1f33071079dc3aaa746e4ad375b7e6c6983fa
Caue Souza <caue.souzapl@gmail.com>
2025-07-30 16:22:56 -0300

    fix(config): correct environment variable names and remove unused appPassword field

--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -1,25 +1,25 @@
 interface Config {
     apiPort: number,
     apiSecret: string,
+    apiPermission: string
     db: {
         port: number,
         username: string,
         password: string,
         name: string
     }
-    appPassword: string
     development: boolean
 }
 
 export const config: Config = {
     apiPort: Number(process.env.API_PORT) || 5432,
-    apiSecret: String(process.env.APISECRET),
+    apiSecret: String(process.env.API_SECRET),
+    apiPermission: String(process.env.API_PERMISSION),
     db: {
         port: Number(process.env.DATABASE_PORT) || 3000,
         username: String(process.env.DATABASE_USERNAME),
         password: String(process.env.DATABASE_PASSWORD),
         name: String(process.env.DATABASE_NAME)
     },
-    appPassword: String(process.env.APP_PASSWORD),
     development: process.env.NODE_ENV === "development",
 }
