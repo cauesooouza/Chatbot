91774126a39a51eafc853aa3fa765ad6a8641888
Caue Souza <caue.souzapl@gmail.com>
2025-07-30 16:24:26 -0300

    feat(routes): add validatePermission middleware to lead and user creation routes

--- api/src/routes/lead.routes.ts
+++ api/src/routes/lead.routes.ts
@@ -3,15 +3,16 @@ import { LeadController } from "../modules/Lead/lead.controller.js";
 import { validateInput } from "../middleware/validateInput.js";
 import { createLeadSchema, updateLeadSchema } from "../modules/Lead/lead.schema.js";
 import { sessionAuth } from "../middleware/sessionAuth.js";
+import { validatePermission } from "../middleware/validatePermission.js";
 
 const leadController = new LeadController();
 const router = Router();
 
-router.post('/new', validateInput(createLeadSchema), leadController.create);
+router.post('/new', validateInput(createLeadSchema), validatePermission, leadController.create);
 // router.get('/dashboard', )
 router.get('/all', sessionAuth, leadController.get);
 router.get('/:id', sessionAuth, leadController.getById);
-router.patch('/update', validateInput(updateLeadSchema), leadController.update);
+router.patch('/update', validateInput(updateLeadSchema), validatePermission, leadController.update);
 router.delete('/delete/:id', sessionAuth, leadController.delete);
 
 export default router;
--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { UserController } from "../modules/User/user.controller.js";
 import { validateInput } from "../middleware/validateInput.js";
 import { createUserSchema } from "../modules/User/user.schema.js";
+import { validatePermission } from "../middleware/validatePermission.js";
 
 const userController = new UserController();
 
 const router = Router();
-router.post('/new', validateInput(createUserSchema), userController.create);
+router.post('/new', validateInput(createUserSchema), validatePermission, userController.create);
 router.get('/all', userController.get);
 router.get('/', userController.getByEmail);
 
