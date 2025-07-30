f511bcc8da551e90e3a8029014933903b34cbe65
Caue Souza <caue.souzapl@gmail.com>
2025-07-30 16:24:16 -0300

    feat(middleware): add validatePermission middleware for permission validation

---
+++ api/src/middleware/validatePermission.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import { config } from "../config/config.js";
+import { GenericError, HttpStatus } from "../errors/GenericError.js";
+
+export const validatePermission = (req: Request, _res: Response, next: NextFunction) => {
+    if (req.body.permission !== config.apiPermission) {
+        throw new GenericError("Permission denied", HttpStatus.FORBIDDEN);
+    }
+    next()
+}
