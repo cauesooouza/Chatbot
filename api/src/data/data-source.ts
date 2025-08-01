9a6f3e0ea92425d13da55a3f3171f35da4d43fd2
Caue Souza <caue.souzapl@gmail.com>
2025-07-31 21:43:46 -0300

    refactor(data-source): update database configuration to use dynamic values from config

--- api/src/data/data-source.ts
+++ api/src/data/data-source.ts
@@ -1,16 +1,16 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { config } from "../config/config.js";
 import { Lead } from "../modules/Lead/lead.entity.js";
 import { User } from "../modules/User/user.entity.js";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
+    type: config.db.type as any,
+    host: config.db.host,
     port: config.db.port,
     username: config.db.username,
     password: config.db.password,
     database: config.db.name,
-    logging: config.development,
-    synchronize: true,
+    logging: config.logging,
+    synchronize: config.synchronize,
     entities: [Lead, User],
 })
