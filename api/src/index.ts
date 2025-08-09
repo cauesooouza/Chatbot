25106084c0ab395389e12c1c1ad4d6e7d71c8588
Caue Souza <caue.souzapl@gmail.com>
2025-08-09 20:15:37 -0300

    refactor(index): remove json middleware and update server port configuration

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,19 +1,19 @@
-import express, { Application, json } from 'express'
+import express, { Application } from 'express'
 import { AppDataSource } from './data/data-source.js';
 import leadRoutes from './routes/lead.routes.js';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import { errorHandler } from './middleware/errors/errorHandler.js';
+import { config } from './config/config.js';
 
 const app: Application = express();
-app.use(json({ limit: '500b' }));
 app.use('/lead', leadRoutes);
 app.use('/user', userRoutes);
 app.use('/auth', authRoutes);
 app.use(errorHandler);
 
 AppDataSource.initialize().then(() => {
-    app.listen(3000, () => {
-        console.info(`server is running at 3000 port`)
+    app.listen(config.apiPort, () => {
+        console.info(`server is running at ${config.apiPort} port`)
     });
 })
